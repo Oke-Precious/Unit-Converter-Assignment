1ad7d86befeeb73f4bd79263b8d6268298b2d358
Oke Precious <okepreciousab@gmail.com>
2026-05-07 23:32:08 -1100

    Refactor input fields in index.html and add conversion logic in script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert("working")
+// document.getElementById("enterCelsius").value = celsius;
+// let celsius = parseFloat(document.getElementById("enterCelsius").value);
+// let fahrenheit = parseFloat(document.getElementById("enterFahrenheit").value);
+// let kilometers = parseFloat(document.getElementById("enterKilometers").value);
+// let miles = parseFloat(document.getElementById("enterMiles").value);
+
+const convertCelsius = () => {
+    celsius = parseFloat(document.getElementById("enterCelsius").value);
+    fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("enterFahrenheit").value = fahrenheit;
+    console.log(fahrenheit);
+}
+const convertFahrenheit = () => {
+    fahrenheit = parseFloat(document.getElementById("enterFahrenheit").value);
+    celsius = (fahrenheit - 32) * 5/9;
+    document.getElementById("enterCelsius").value = celsius;
+    console.log(celsius);
+}
